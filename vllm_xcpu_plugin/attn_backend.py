f613dd21f81326bcfa9de0948e69765b5bdf7083
caowanlu <caowanlu@xcoresigma.com>
2026-02-26 10:27:40 +0000

    [Update] Update TritonAttentionBackend

--- vllm_xcpu_plugin/attn_backend.py
+++ vllm_xcpu_plugin/attn_backend.py
@@ -1,8 +1,5 @@
-from dataclasses import dataclass
 
 import torch
-from vllm.config import VllmConfig
-from vllm.utils.math_utils import next_power_of_2
 from vllm.v1.attention.backend import AttentionType
 from vllm.v1.attention.backends.registry import (
     AttentionBackendEnum,
@@ -12,106 +9,7 @@ from vllm.v1.attention.backends.triton_attn import (
     TritonAttentionBackend,
     TritonAttentionImpl,
     TritonAttentionMetadata,
-    TritonAttentionMetadataBuilder,
 )
-from vllm.v1.attention.backends.utils import CommonAttentionMetadata
-from vllm.v1.kv_cache_interface import AttentionSpec
-
-MAX_TILE_SIZE_2D = 32
-MIN_BLOCK_M_2D = 16
-
-
-@dataclass
-class XcpuTritonAttentionMetadata(TritonAttentionMetadata):
-    # cpp ops
-    workspace: torch.Tensor | None = None
-
-
-class XcpuTritonAttentionMetadataBuilder(TritonAttentionMetadataBuilder):
-    def __init__(
-        self,
-        kv_cache_spec: AttentionSpec,
-        layer_names: list[str],
-        vllm_config: VllmConfig,
-        device: torch.device,
-    ):
-        super().__init__(kv_cache_spec, layer_names, vllm_config, device)
-
-        num_queries_per_kv = self.num_heads_q // self.num_heads_kv
-        self.block_m = (
-            MIN_BLOCK_M_2D
-            if num_queries_per_kv < MIN_BLOCK_M_2D
-            else next_power_of_2(num_queries_per_kv)
-        )
-        self.max_tile_size = MAX_TILE_SIZE_2D
-        max_elem_size = 4
-        import torch_xcpu
-
-        dummy = torch.empty(0)
-        workspace_size = torch_xcpu.ops.unified_attention_get_workspace_size(
-            dummy,
-            max_elem_size,
-            self.block_m,
-            self.max_tile_size,
-            next_power_of_2(self.headdim),
-        )
-        self.workspace = torch.empty(workspace_size, dtype=torch.uint8, device=device)
-
-    def build(
-        self,
-        common_prefix_len: int,
-        common_attn_metadata: CommonAttentionMetadata,
-        fast_build: bool = False,
-    ) -> XcpuTritonAttentionMetadata:
-        num_actual_tokens = common_attn_metadata.num_actual_tokens
-        max_query_len = common_attn_metadata.max_query_len
-
-        max_seq_len = common_attn_metadata.max_seq_len
-        query_start_loc = common_attn_metadata.query_start_loc
-        seq_lens = common_attn_metadata.seq_lens
-        block_table_tensor = common_attn_metadata.block_table_tensor
-        slot_mapping = common_attn_metadata.slot_mapping
-
-        use_cascade = common_prefix_len > 0
-
-        if use_cascade:
-            cu_prefix_query_lens = torch.tensor(
-                [0, num_actual_tokens], dtype=torch.int32, device=self.device
-            )
-            prefix_kv_lens = torch.tensor(
-                [common_prefix_len], dtype=torch.int32, device=self.device
-            )
-            suffix_kv_lens = common_attn_metadata.seq_lens.cpu() - common_prefix_len
-            suffix_kv_lens = suffix_kv_lens.to(self.device)
-        else:
-            cu_prefix_query_lens = None
-            prefix_kv_lens = None
-            suffix_kv_lens = None
-            prefix_scheduler_metadata = None
-
-        attn_metadata = XcpuTritonAttentionMetadata(
-            num_actual_tokens=num_actual_tokens,
-            max_query_len=max_query_len,
-            query_start_loc=query_start_loc,
-            max_seq_len=max_seq_len,
-            seq_lens=seq_lens,
-            block_table=block_table_tensor,
-            slot_mapping=slot_mapping,
-            use_cascade=use_cascade,
-            common_prefix_len=common_prefix_len,
-            cu_prefix_query_lens=cu_prefix_query_lens,
-            prefix_kv_lens=prefix_kv_lens,
-            suffix_kv_lens=suffix_kv_lens,
-            prefix_scheduler_metadata=prefix_scheduler_metadata,
-            seq_threshold_3D=self.seq_threshold_3D,
-            num_par_softmax_segments=self.num_par_softmax_segments,
-            softmax_segm_output=self.softmax_segm_output,
-            softmax_segm_max=self.softmax_segm_max,
-            softmax_segm_expsum=self.softmax_segm_expsum,
-            workspace=self.workspace,
-        )
-
-        return attn_metadata
 
 
 @register_backend(AttentionBackendEnum.TRITON_ATTN)
@@ -119,10 +17,6 @@ class XcpuTritonAttentionBackend(TritonAttentionBackend):
     @staticmethod
     def get_impl_cls() -> type["XcpuTritonAttentionImpl"]:
         return XcpuTritonAttentionImpl
-
-    @staticmethod
-    def get_builder_cls() -> type["XcpuTritonAttentionMetadataBuilder"]:
-        return XcpuTritonAttentionMetadataBuilder
 
 
 class XcpuTritonAttentionImpl(TritonAttentionImpl):
@@ -249,7 +143,6 @@ class XcpuTritonAttentionImpl(TritonAttentionImpl):
             softmax_scale=self.scale,
             window_size=1 + self.sliding_window[0],
             block_table=block_table,
-            workspace=attn_metadata.workspace,
             # seq_threshold_3D=seq_threshold_3D,
             # num_par_softmax_segments=num_par_softmax_segments,
             # softmax_segm_output=softmax_segm_output,
