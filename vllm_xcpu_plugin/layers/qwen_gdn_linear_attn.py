177f8ca8cdf39b8590028fdc99d10481030b9bd0
Claire <31379115+Claireeeee@users.noreply.github.com>
2026-07-21 18:41:58 +0800

    [GDN] Update chunk_gated_delta_rule to avoid data copy (#20)
    更新 XcpuChunkGatedDeltaRule 插件，新增对算子 chunk_gated_delta_rule_separated_custom_v2 的调用路径，以配合支持 vllm 0.24 中调用处的优化
    
    Co-authored-by: caowanlu <caowanlu@xcoresigma.com>
    Co-authored-by: kunzh <zhikun.wu@outlook.com>

--- vllm_xcpu_plugin/layers/qwen_gdn_linear_attn.py
+++ vllm_xcpu_plugin/layers/qwen_gdn_linear_attn.py
@@ -17,18 +17,56 @@ class XcpuChunkGatedDeltaRule(ChunkGatedDeltaRule):
         v: torch.Tensor,
         g: torch.Tensor,
         beta: torch.Tensor,
-        initial_state: torch.Tensor,
-        output_final_state: bool,
+        initial_state: torch.Tensor | None = None,
+        output_final_state: bool = True,
         cu_seqlens: torch.Tensor | None = None,
         chunk_indices: torch.Tensor | None = None,
         chunk_offsets: torch.Tensor | None = None,
         use_qk_l2norm_in_kernel: bool = True,
         core_attn_out: torch.Tensor | None = None,
+        ssm_state: torch.Tensor | None = None,
+        ssm_state_indices: torch.Tensor | None = None,
+        has_initial_state: torch.Tensor | None = None,
     ):
         import torch_xcpu
 
         assert cu_seqlens is not None
-        output, final_state = torch_xcpu.ops.chunk_gated_delta_rule(
+        del chunk_indices, chunk_offsets, core_attn_out
+
+        if ssm_state is not None:
+            assert initial_state is None
+            assert output_final_state
+            assert ssm_state_indices is not None
+            assert has_initial_state is not None
+            _, HV, V, K = ssm_state.shape
+            T = q.shape[1]
+            workspace = self._allocate_workspace(q, cu_seqlens, HV, V, K, T)
+            output = torch_xcpu.ops.chunk_gated_delta_rule_separated_custom_v2(
+                q=q,
+                k=k,
+                v=v,
+                g=g,
+                beta=beta,
+                ssm_state=ssm_state,
+                ssm_state_indices=ssm_state_indices,
+                has_initial_state=has_initial_state,
+                cu_seqlens=cu_seqlens,
+                use_qk_l2norm_in_kernel=use_qk_l2norm_in_kernel,
+                workspace=workspace,
+            )
+            return output, None
+
+        assert initial_state is not None
+        assert ssm_state_indices is None
+        assert has_initial_state is None
+        B, T, H, K = q.shape
+        HV = v.shape[2]
+        V = v.shape[3]
+        assert B == 1
+        del H
+        workspace = self._allocate_workspace(q, cu_seqlens, HV, V, K, T)
+
+        return torch_xcpu.ops.chunk_gated_delta_rule_separated(
             q=q,
             k=k,
             v=v,
@@ -38,9 +76,28 @@ class XcpuChunkGatedDeltaRule(ChunkGatedDeltaRule):
             output_final_state=output_final_state,
             cu_seqlens=cu_seqlens,
             use_qk_l2norm_in_kernel=use_qk_l2norm_in_kernel,
+            workspace=workspace,
         )
-        if core_attn_out is not None:
-            output_flat = output.squeeze(0).reshape(-1)
-            core_attn_out_flat = core_attn_out.reshape(-1)
-            core_attn_out_flat[: output_flat.numel()].copy_(output_flat)
-        return output, final_state
+
+    @staticmethod
+    def _allocate_workspace(
+        q: torch.Tensor,
+        cu_seqlens: torch.Tensor,
+        num_v_heads: int,
+        value_dim: int,
+        key_dim: int,
+        num_tokens: int,
+    ) -> torch.Tensor:
+        num_seqs = cu_seqlens.shape[0] - 1
+        C = 64
+        max_chunk_num = (num_tokens + C - 1) / C + num_seqs
+        max_workspace_size = max_chunk_num * (
+            num_v_heads
+            * (C * value_dim * 2 + C * C + key_dim * value_dim + C * key_dim)
+            + 3 * 2
+        )
+        return torch.empty(
+            int(max_workspace_size),
+            dtype=q.dtype,
+            device=q.device,
+        )
